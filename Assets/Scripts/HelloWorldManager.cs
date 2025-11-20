9bfb1d11a921b3409d335a5b1fc9580c29dd134d
luyumenq <72938590+hubertYumengLu@users.noreply.github.com>
2025-11-20 23:21:03 +0100

    Followed netcode tutorial

--- Assets/Scripts/HelloWorldManager.cs
+++ Assets/Scripts/HelloWorldManager.cs
@@ -1,67 +1,153 @@
-// using Unity.Netcode;
-// using UnityEngine;
+using System;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UIElements;
 
-// namespace HelloWorld
-// {
-//     public class HelloWorldManager : MonoBehaviour
-//     {
-//         private NetworkManager m_NetworkManager;
+namespace HelloWorld
+{
+    public class HelloWorldManager : MonoBehaviour
+    {
+        VisualElement rootVisualElement;
+        Button hostButton;
+        Button clientButton;
+        Button serverButton;
+        Button moveButton;
+        Label statusLabel;
 
-//         void Awake()
-//         {
-//             m_NetworkManager = GetComponent<NetworkManager>();
-//         }
+        void OnEnable()
+        {
+            var uiDocument = GetComponent<UIDocument>();
+            rootVisualElement = uiDocument.rootVisualElement;
 
-//         void OnGUI()
-//         {
-//             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-//             if (!m_NetworkManager.IsClient && !m_NetworkManager.IsServer)
-//             {
-//                 StartButtons();
-//             }
-//             else
-//             {
-//                 StatusLabels();
+            hostButton = CreateButton("HostButton", "Host");
+            clientButton = CreateButton("ClientButton", "Client");
+            serverButton = CreateButton("ServerButton", "Server");
+            moveButton = CreateButton("MoveButton", "Move");
+            statusLabel = CreateLabel("StatusLabel", "Not Connected");
 
-//                 SubmitNewPosition();
-//             }
+            rootVisualElement.Clear();
+            rootVisualElement.Add(hostButton);
+            rootVisualElement.Add(clientButton);
+            rootVisualElement.Add(serverButton);
+            rootVisualElement.Add(moveButton);
+            rootVisualElement.Add(statusLabel);
 
-//             GUILayout.EndArea();
-//         }
+            hostButton.clicked += OnHostButtonClicked;
+            clientButton.clicked += OnClientButtonClicked;
+            serverButton.clicked += OnServerButtonClicked;
+            moveButton.clicked += SubmitNewPosition;
+        }
 
-//         static void StartButtons()
-//         {
-//             if (GUILayout.Button("Host")) m_NetworkManager.StartHost();
-//             if (GUILayout.Button("Client")) m_NetworkManager.StartClient();
-//             if (GUILayout.Button("Server")) m_NetworkManager.StartServer();
-//         }
+        void Update()
+        {
+            UpdateUI();
+        }
 
-//         static void StatusLabels()
-//         {
-//             var mode = m_NetworkManager.IsHost ?
-//                 "Host" : m_NetworkManager.IsServer ? "Server" : "Client";
+        void OnDisable()
+        {
+            hostButton.clicked -= OnHostButtonClicked;
+            clientButton.clicked -= OnClientButtonClicked;
+            serverButton.clicked -= OnServerButtonClicked;
+            moveButton.clicked -= SubmitNewPosition;
+        }
 
-//             GUILayout.Label("Transport: " +
-//                 m_NetworkManager.NetworkConfig.NetworkTransport.GetType().Name);
-//             GUILayout.Label("Mode: " + mode);
-//         }
+        void OnHostButtonClicked() => NetworkManager.Singleton.StartHost();
 
-//         static void SubmitNewPosition()
-//         {
-//             if (GUILayout.Button(m_NetworkManager.IsServer ? "Move" : "Request Position Change"))
-//             {
-//                 if (m_NetworkManager.IsServer && !m_NetworkManager.IsClient)
-//                 {
-//                     foreach (ulong uid in m_NetworkManager.ConnectedClientsIds)
-//                         m_NetworkManager.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<HelloWorldPlayer>().Move();
-//                 }
-//                 else
-//                 {
-//                     var playerObject = m_NetworkManager.SpawnManager.GetLocalPlayerObject();
-//                     var player = playerObject.GetComponent<HelloWorldPlayer>();
-//                     player.Move();
-//                 }
-//             }
-//         }
-//     }
-// }
+        void OnClientButtonClicked() => NetworkManager.Singleton.StartClient();
+
+        void OnServerButtonClicked() => NetworkManager.Singleton.StartServer();
+
+        // Disclaimer: This is not the recommended way to create and stylize the UI elements, it is only utilized for the sake of simplicity.
+        // The recommended way is to use UXML and USS. Please see this link for more information: https://docs.unity3d.com/Manual/UIE-USS.html
+        private Button CreateButton(string name, string text)
+        {
+            var button = new Button();
+            button.name = name;
+            button.text = text;
+            button.style.width = 240;
+            button.style.backgroundColor = Color.white;
+            button.style.color = Color.black;
+            button.style.unityFontStyleAndWeight = FontStyle.Bold;
+            return button;
+        }
+
+        private Label CreateLabel(string name, string content)
+        {
+            var label = new Label();
+            label.name = name;
+            label.text = content;
+            label.style.color = Color.black;
+            label.style.fontSize = 18;
+            return label;
+        }
+
+        void UpdateUI()
+        {
+            if (NetworkManager.Singleton == null)
+            {
+                SetStartButtons(false);
+                SetMoveButton(false);
+                SetStatusText("NetworkManager not found");
+                return;
+            }
+
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            {
+                SetStartButtons(true);
+                SetMoveButton(false);
+                SetStatusText("Not connected");
+            }
+            else
+            {
+                SetStartButtons(false);
+                SetMoveButton(true);
+                UpdateStatusLabels();
+            }
+        }
+
+        void SetStartButtons(bool state)
+        {
+            hostButton.style.display = state ? DisplayStyle.Flex : DisplayStyle.None;
+            clientButton.style.display = state ? DisplayStyle.Flex : DisplayStyle.None;
+            serverButton.style.display = state ? DisplayStyle.Flex : DisplayStyle.None;
+        }
+
+        void SetMoveButton(bool state)
+        {
+            moveButton.style.display = state ? DisplayStyle.Flex : DisplayStyle.None;
+            if (state)
+            {
+                moveButton.text = NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change";
+            }
+        }
+
+        void SetStatusText(string text) => statusLabel.text = text;
+
+        void UpdateStatusLabels()
+        {
+            var mode = NetworkManager.Singleton.IsHost ? "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+            string transport = "Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name;
+            string modeText = "Mode: " + mode;
+            SetStatusText($"{transport}\n{modeText}");
+        }
+
+        void SubmitNewPosition()
+        {
+            if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient)
+            {
+                foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
+                {
+                    var playerObject = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid);
+                    var player = playerObject.GetComponent<HelloWorldPlayer>();
+                    player.Move();
+                }
+            }
+            else if (NetworkManager.Singleton.IsClient)
+            {
+                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+                var player = playerObject.GetComponent<HelloWorldPlayer>();
+                player.Move();
+            }
+        }
+    }
+}
