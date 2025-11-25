6a07491cdfb837b55d046a762bd6d703f61f78f5
Yumeng Lu <72938590+luyumenq@users.noreply.github.com>
2025-11-25 18:03:52 +0100

    update: user prefab movable with arrow keys

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : NetworkBehaviour
     private void Update()
     {
         if (!IsOwner) return;
-        var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _transform.transform.position += movement * speed * Time.deltaTime;
     }
 
